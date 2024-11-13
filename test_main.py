e0e3afbd0188ae3725258e9abb4a9e63ab07f217
LovisaP <147035984+LovisaP@users.noreply.github.com>
2024-11-13 10:17:06 +0100

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -2,15 +2,15 @@ import pytest
 from main import data_SMHI
 
 def test_api_response():
-    df, code = data_SMHI()
-    
-    # Kontrollera att API-anropet lyckades
-    assert code == 200, f"Expected status code 200, but got {code}"
-    
-    # Kontrollera att dataformatet är korrekt
+
+    df = data_SMHI()
+
+
     assert not df.empty, "DataFrame should not be empty"
+
+
     assert "temperature" in df.columns, "DataFrame should contain 'temperature' column"
     assert "rainOrSnow" in df.columns, "DataFrame should contain 'rainOrSnow' column"
 
-    print("API response and data format are correct")
+    print("Data format and content are correct")
 
